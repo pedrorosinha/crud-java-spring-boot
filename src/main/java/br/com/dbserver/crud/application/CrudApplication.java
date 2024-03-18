fbcd581b028754d21b442c67d1249b5aa7ab351c
pedro.rosinha <pedro.rosinha@VIVOTECH-06.dbserver.com.br>
2024-03-17 21:02:44 -0300

    Adicionando a pasta application

--- src/main/java/br/com/dbserver/crud/application/CrudApplication.java
+++ src/main/java/br/com/dbserver/crud/application/CrudApplication.java
@@ -1,4 +1,4 @@
-package br.com.dbserver.crud;
+package br.com.dbserver.crud.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
